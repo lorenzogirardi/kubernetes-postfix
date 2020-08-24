740060d8a54734c538f20ac30cf5c6946b715fa1
lgirardi <lgirardi@fujin.h4x0r3d.lan>
2020-08-24 20:17:46 +0200

    starting point docker and kubernetes

---
+++ docker/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#check
+if [[ -a /etc/supervisor/conf.d/supervisord.conf ]]; then
+  exit 0
+fi
+
+#supervisor
+cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
+[supervisord]
+nodaemon=true
+
+[program:postfix]
+command=/opt/postfix.sh
+
+[program:rsyslog]
+command=/usr/sbin/rsyslogd -n 
+EOF
+
+############
+#  postfix
+############
+cat >> /opt/postfix.sh <<EOF
+#!/bin/bash
+service postfix start
+tail -f /var/log/mail.log
+EOF
+chmod +x /opt/postfix.sh
+postconf -F '*/*/chroot = n'
+
+
+
+#############
+#  opendkim
+#############
+
+if [[ -z "$(find /etc/opendkim/domainkeys -iname *.private)" ]]; then
+  exit 0
+fi
+cat >> /etc/supervisor/conf.d/supervisord.conf <<EOF
+
+[program:opendkim]
+command=/usr/sbin/opendkim -f
+EOF
